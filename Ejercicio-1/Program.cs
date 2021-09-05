1070bf69cca062f9d5763a3cfa8f3e6020ec2005
Maximiliano Sosa <msosa_@hotmail.com>
2021-09-05 14:48:19 -0300

    Add project files.

---
+++ Ejercicio-1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ejercicio_1
+{
+    class Program
+    {
+        static int findMin(int[] arr)
+        {
+            int min = arr[1];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int current = arr[i + 1];
+                if (min < current)
+                {
+                    min = current;
+                }
+            }
+            return min;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 8, 20, 2, 3 };
+            integer min = findMin(numbers);
+            Console.WriteLine(min);
+        }
+    }
+}
