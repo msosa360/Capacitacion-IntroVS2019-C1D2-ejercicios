c73908d4719e5ce461ba54c6a7d368bad779e8a6
Maximiliano Sosa <msosa_@hotmail.com>
2021-09-05 15:11:36 -0300

    mater

--- Ejercicio-1/Program.cs
+++ Ejercicio-1/Program.cs
@@ -2,25 +2,8 @@
 {
     class Program
     {
-        static int findMin(int[] arr)
-        {
-            int min = arr[1];
-            for (int i = 0; i < arr.Length; i++)
-            {
-                int current = arr[i + 1];
-                if (min < current)
-                {
-                    min = current;
-                }
-            }
-            return min;
-        }
-
         static void Main(string[] args)
         {
-            int[] numbers = { 1, 8, 20, 2, 3 };
-            integer min = findMin(numbers);
-            Console.WriteLine(min);
         }
     }
 }
